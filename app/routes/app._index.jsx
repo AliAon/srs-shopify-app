42a9b4ddc4fd45aba69aa2f042a6775ad3e4c4b2
Aon Mohammad <aon094944@gmail.com>
2026-03-06 19:37:22 +0500

    redirext set

--- app/routes/app._index.jsx
+++ app/routes/app._index.jsx
@@ -1,9 +1,23 @@
 import { boundary } from "@shopify/shopify-app-react-router/server";
+import { authenticate } from "../shopify.server";
+import { useLoaderData } from "react-router";
+
+export const loader = async ({ request }) => {
+  const { admin } = await authenticate.admin(request);
+  const response = await admin.graphql(`query { shop { 
+    name,
+     myshopifyDomain,
+        url,
+        email, } }`);
+  const api = await response.json();
+  return api.data.shop;
+};
 
 export default function Index() {
+  const response = useLoaderData();
   const handleConnectApp = () => {
     window.open(
-      "https://retail-system-overview.vercel.app/authenticate",
+      `https://retail-system-overview.vercel.app/authenticate?shop=${response.myshopifyDomain}&email=${response.email}&appUrl=${response.url}`,
       "_top",
     );
   };
